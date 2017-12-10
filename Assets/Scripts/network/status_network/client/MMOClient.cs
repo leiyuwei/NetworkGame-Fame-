a9edac5a7c5624b51c83a5547a093d6901b1f4a4
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-12-10 16:34:57 +0900

    no message

--- Assets/Scripts/network/status_network/client/MMOClient.cs
+++ Assets/Scripts/network/status_network/client/MMOClient.cs
@@ -10,6 +10,7 @@ namespace MMO
 	{
 		NetworkClient client;
 		UnityAction<NetworkMessage> onConnect;
+		UnityAction<NetworkMessage> onRecieveMessage;
 
 		void Start ()
 		{
@@ -22,18 +23,26 @@ namespace MMO
 		void Update(){
 			if(Input.GetKeyDown(KeyCode.H)){
 //				client.Connect ();
-				Connect("127.0.0.1",NetConstant.LISTENE_PORT,null);
+				Connect("127.0.0.1",NetConstant.LISTENE_PORT,null,null);
+			}
+
+			if(Input.GetKeyDown(KeyCode.J)){
+				SendMessage ();
 			}
 		}
 
 		public void SendMessage(){
-		
+			PlayerData data = new PlayerData ();
+			data.playerForward = Vector3.forward;
+			data.playerPosition = Vector3.zero;
+			client.Send (MessageConstant.CLIENT_TO_SERVER_MSG,data);
 		}
 
-		public void Connect (string ip, int port, UnityAction<NetworkMessage> onConnect)
+		public void Connect (string ip, int port, UnityAction<NetworkMessage> onConnect,UnityAction<NetworkMessage> onRecieveMessage)
 		{
 			Debug.Log (string.Format ("{0},{1}", ip, port));
 			this.onConnect = onConnect;
+			this.onRecieveMessage = onRecieveMessage;
 			client.Connect (ip, port);
 		}
 
@@ -50,8 +59,9 @@ namespace MMO
 			//			BattleClientController.Instance.Reset ();
 		}
 
-		void OnRecieveMessage(NetworkMessage nm){
-			
+		void OnRecieveMessage(NetworkMessage msg){
+			TransferData playerHandle = msg.ReadMessage<TransferData> ();
+			Debug.Log (JsonUtility.ToJson(playerHandle));
 		}
 
 	}
